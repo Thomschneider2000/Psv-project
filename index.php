ebf0c3ab7b2eeebed231ee7ebba204e245828621
Thom Schneider <85222689+Thomschneider2000@users.noreply.github.com>
2023-06-14 21:43:19 +0200

    added new styling to index
    added new styling to index

--- index.php
+++ index.php
@@ -1,140 +1,134 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Document</title>
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
-		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-		<link rel="stylesheet" href="styles.css">
-	</head>
-	<body>
-		<div class="row">
-			<div class="top-nav">
-				<a href="#"><i class="bi bi-person-circle"></i></a>
-				<a href="#"><i class="bi bi-sliders"></i></a>
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+		integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+		crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+	<link rel="stylesheet" href="styles.css">
+	<link rel="stylesheet" href="styles-hristo.css">
+</head>
+
+<body>
+	<header class="header">
+		<?php include "nav.php" ?>
+
+		<div class="container">
+			<div id="cover">
+				<a href="#"><img src="https://i.imgur.com/dkSil46.png"></a>
+				<a href="#"><img style="padding:10px;"src="https://i.imgur.com/ouUcQO3.png"></a>
+				<a href="#"><img style="padding:10px;" src="https://i.imgur.com/pThgOcq.png"></a>
+				<a href="#"><img src="https://i.imgur.com/JCxRAhT.jpg"></a>
+			</div>
+			<div class="match-container">
+				<div class="Header">
+					<div class="Header_TeamLeft">
+						<div class="Team_Name">Psv</div>
+					</div>
+					<div class="Header_Scores">
+						<div class="Scores">
+							<div class="Scores_Score">
+								<div class="Score">2</div>
+							</div>
+							<div class="Scores_Score">
+								<div class="Score">1</div>
+							</div>
+						</div>
+					</div>
+					<div class="Header_TeamRight">
+						<div class="Team_Name">FC Emmen</div>
+					</div>
+				</div>
+			</div>
+			<div class="match-container">
+				<div class="Header">
+					<div class="Header_TeamLeft">
+						<div class="Team_Name">Ajax</div>
+					</div>
+					<div class="Header_Scores">
+						<div class="Scores">
+							<div class="Scores_Score">
+								<div class="Score">1</div>
+							</div>
+							<div class="Scores_Score">
+								<div class="Score">0</div>
+							</div>
+						</div>
+					</div>
+					<div class="Header_TeamRight">
+						<div class="Team_Name">Roda JC</div>
+					</div>
+				</div>
+			</div>
+			<div class="match-container">
+				<div class="Header">
+					<div class="Header_TeamLeft">
+						<div class="Team_Name">FC Utrecht</div>
+					</div>
+					<div class="Header_Scores">
+						<div class="Scores">
+							<div class="Scores_Score">
+								<div class="Score">0</div>
+							</div>
+							<div class="Scores_Score">
+								<div class="Score">2</div>
+							</div>
+						</div>
+					</div>
+					<div class="Header_TeamRight">
+						<div class="Team_Name">AZ</div>
+					</div>
+				</div>
+			</div>
+			<div class="match-container">
+				<div class="Header">
+					<div class="Header_TeamLeft">
+						<div class="Team_Name">Top Oss</div>
+					</div>
+					<div class="Header_Scores">
+						<div class="Scores">
+							<div class="Scores_Score">
+								<div class="Score">1</div>
+							</div>
+							<div class="Scores_Score">
+								<div class="Score">0</div>
+							</div>
+						</div>
+					</div>
+					<div class="Header_TeamRight">
+						<div class="Team_Name">Almere City</div>
+					</div>
+				</div>
+			</div>
+			<div class="match-container">
+				<div class="Header">
+					<div class="Header_TeamLeft">
+						<div class="Team_Name">FC Volendam</div>
+					</div>
+					<div class="Header_Scores">
+						<div class="Scores">
+							<div class="Scores_Score">
+								<div class="Score">0</div>
+							</div>
+							<div class="Scores_Score">
+								<div class="Score">0</div>
+							</div>
+						</div>
+					</div>
+					<div class="Header_TeamRight">
+						<div class="Team_Name">Excelsior</div>
+					</div>
+				</div>
 			</div>
 		</div>
-		<header class="header">
-    <?php include "nav.php"?>
+	</header>
+</body>
+<script src="assets/js/nav.js"></script>
 
-			<div class="row">
-				<div class="user-name">
-					<h1>Steven Berg</h1>
-				</div>
-			</div>
-
-			<div class="container">
-				<div class="grid">
-					<a class="player" href="#">Heatmap</a>
-					<a class="player" href="#">Grid</a>
-					<a class="player" href="#">Woodworks</a>
-					<a class="player" href="#">Classic</a>
-				</div>
-				<div class="match-container">
-					<div class="Header">
-						<div class="Header_TeamLeft">
-							<div class="Team_Name">Psv</div>
-						</div>
-						<div class="Header_Scores">
-							<div class="Scores">
-								<div class="Scores_Score">
-									<div class="Score">2</div>
-								</div>
-								<div class="Scores_Score">
-									<div class="Score">1</div>
-								</div>
-							</div>
-						</div>
-						<div class="Header_TeamRight">
-							<div class="Team_Name">FC Emmen</div>
-						</div>
-					</div>
-				</div>
-				<div class="match-container">
-					<div class="Header">
-						<div class="Header_TeamLeft">
-							<div class="Team_Name">Ajax</div>
-						</div>
-						<div class="Header_Scores">
-							<div class="Scores">
-								<div class="Scores_Score">
-									<div class="Score">1</div>
-								</div>
-								<div class="Scores_Score">
-									<div class="Score">0</div>
-								</div>
-							</div>
-						</div>
-						<div class="Header_TeamRight">
-							<div class="Team_Name">Roda JC</div>
-						</div>
-					</div>
-				</div>
-				<div class="match-container">
-					<div class="Header">
-						<div class="Header_TeamLeft">
-							<div class="Team_Name">FC Utrecht</div>
-						</div>
-						<div class="Header_Scores">
-							<div class="Scores">
-								<div class="Scores_Score">
-									<div class="Score">0</div>
-								</div>
-								<div class="Scores_Score">
-									<div class="Score">2</div>
-								</div>
-							</div>
-						</div>
-						<div class="Header_TeamRight">
-							<div class="Team_Name">AZ</div>
-						</div>
-					</div>
-				</div>
-				<div class="match-container">
-					<div class="Header">
-						<div class="Header_TeamLeft">
-							<div class="Team_Name">Top Oss</div>
-						</div>
-						<div class="Header_Scores">
-							<div class="Scores">
-								<div class="Scores_Score">
-									<div class="Score">1</div>
-								</div>
-								<div class="Scores_Score">
-									<div class="Score">0</div>
-								</div>
-							</div>
-						</div>
-						<div class="Header_TeamRight">
-							<div class="Team_Name">Almere City</div>
-						</div>
-					</div>
-				</div>
-				<div class="match-container">
-					<div class="Header">
-						<div class="Header_TeamLeft">
-							<div class="Team_Name">FC Volendam</div>
-						</div>
-						<div class="Header_Scores">
-							<div class="Scores">
-								<div class="Scores_Score">
-									<div class="Score">0</div>
-								</div>
-								<div class="Scores_Score">
-									<div class="Score">0</div>
-								</div>
-							</div>
-						</div>
-						<div class="Header_TeamRight">
-							<div class="Team_Name">Excelsior</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</header>
-	</body>
-	<script src="assets/js/nav.js"></script>
 </html>
